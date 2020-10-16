0f0e4040a1bc3e23e7461147e46a32d108f7521d
Kartikey Bhawsar <53837332+kartikey-bhawsar@users.noreply.github.com>
2020-10-16 09:11:36 +0530

    addition
    program to add 2 numbers

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <int>
+#define vlli vector <long long int>
+#define lli long long int
+//#define m 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a=10,b=20;
+    int sum=a+b;
+    cout<<sum<<endl;
+    return 0;
+}
