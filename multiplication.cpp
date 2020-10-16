8f4c814a484c06d3425a597e8d57eb839ae08297
Kartikey Bhawsar <53837332+kartikey-bhawsar@users.noreply.github.com>
2020-10-16 09:14:47 +0530

    multiplication
    program to multiply 2 numbers

---
+++ multiplication.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector <int>
+#define vlli vector <long long int>
+#define lli long long int
+//#define m 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t,i;
+    cin>>t;
+    while(t--)
+    {
+        int a=1;
+        int b=4;
+        int mul=a*b;
+        cout<<mul<<endl;
+    }
+}
